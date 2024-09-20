6a6802d9e34fd41db723f169d9a4c9cc4713686a
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2024-09-20 15:54:57 +0200

    Remove lining that was ignoring Publish folder

---
+++ Betting.Application/Commands/CasinoWagers/Publish/PublishCasinoWagerCommand.cs
@@ -0,0 +1,42 @@
+﻿using Betting.Application.Interfaces;
+using MediatR;
+
+namespace Betting.Application.Commands.CasinoWagers.Publish
+{
+    public class PublishCasinoWagerCommand : IRequest
+    {
+        public Guid WagerId { get; set; }
+        public string Theme { get; set; } = string.Empty;
+        public string Provider { get; set; } = string.Empty;
+        public string GameName { get; set; } = string.Empty;
+        public Guid TransactionId { get; set; }
+        public Guid BrandId { get; set; }
+        public Guid AccountId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public Guid ExternalReferenceId { get; set; }
+        public Guid TransactionTypeId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public int NumberOfBets { get; set; }
+        public string CountryCode { get; set; } = string.Empty;
+        public string SessionData { get; set; } = string.Empty;
+        public long Duration { get; set; }
+
+        public class PublishCasinoWagerCommandHandler : IRequestHandler<PublishCasinoWagerCommand, Unit>
+        {
+            private readonly IMessagePublisher _messagePublisher;
+
+            public PublishCasinoWagerCommandHandler(IMessagePublisher messagePublisher)
+            {
+                _messagePublisher = messagePublisher;
+            }
+
+            public async Task<Unit> Handle(PublishCasinoWagerCommand request, CancellationToken cancellationToken)
+            {
+                _messagePublisher.Publish(request);
+
+                return Unit.Value;
+            }
+        }
+    }
+}
